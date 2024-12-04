6093a6cdf9c725f95254b362ff2a80b9aa7edbb6
Leslie-K <88902804+Ayirileslie@users.noreply.github.com>
2024-12-04 09:56:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,6 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=True)
-#if __name__=='__main__':
+if __name__=='__main__':
     #app.run(host='0.0.0.0',port=8080)
 
